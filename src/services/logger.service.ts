79f7e350423060531c0391ebafbfb427ebe2f577
Rene Rey <rene@stackitgroup.com>
2021-09-22 09:05:57 -0700

    Updated with major changes. Have now introduced 'fundingAmount' to use in conjunction with minimumBalance to deal the binaryAddress accounts.

---
+++ src/services/logger.service.ts
@@ -0,0 +1,14 @@
+import { Logger as TsLogger } from 'tslog';
+import { ApiConfig } from '../api.config';
+
+export class Logger extends TsLogger {
+    constructor(options: object) {
+        super(options);
+    }
+}
+
+export const logger = new Logger({
+    name: 'JIM',
+    prefix: [],
+    minLevel: ApiConfig.loggerLevel
+});
